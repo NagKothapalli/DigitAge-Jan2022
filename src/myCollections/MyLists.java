83a78a9bee2947539cdbc3cd4d13affae4a73285
NagKothapalli <nag022@gmail.com>
2022-02-09 11:38:40 +0530

    Feb9th code pushed

--- src/myCollections/MyLists.java
+++ src/myCollections/MyLists.java
@@ -2,8 +2,11 @@ package myCollections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
-
+import java.util.Map;
+import java.util.TreeSet;
 import org.junit.Test;
 
 public class MyLists
@@ -110,9 +113,106 @@ public class MyLists
 			System.out.print( numbers.get(i) + " , ");
 		}
 	}
+	@Test
+	public void treeSet()
+    {
+    	 TreeSet<String> al=new TreeSet<String>();  
+    	  al.add("Ravi");  
+    	  al.add("Vijay");  
+    	  al.add("Ram");  
+    	  al.add("Ajay");  
+    	  //Traversing elements  
+    	  Iterator<String> itr=al.iterator();  
+    	  while(itr.hasNext())
+    	  {  
+    	   System.out.println(itr.next());  
+    	  }
+    }  
+	@Test
+	public void hashMap()  // no duplicate keys  , put , putAll , putifAbsent , remove - key based , value based , replace
+    {
+		//HashMap<key,value>
+       HashMap<Integer,String> map=new HashMap<Integer,String>();//Creating HashMap    
+	   map.put(1,"Ram");  //Put elements in Map  
+	   map.put(2,"Ajay");    
+	   map.put(3,"Krishna");   
+	   map.put(4,"Ganesh");   
+	   //Create child map
+	   HashMap<Integer,String> cMap=new HashMap<Integer,String>();//Creating HashMap    
+	   map.put(5,"Ramesh");  //Put elements in Map  
+	   map.put(6,"Ajay");    
+	   map.put(7,"RaviKrishna");   
+	   map.put(4,"Ganesh"); 
+	   //---------------------
+	   map.putAll(cMap);
+	   System.out.println("one : " + map.get(3));
+	   System.out.println("Length of the map :" + map.size());
+	   System.out.println("Iterating Hashmap...");  
+		
+	  for(Map.Entry m : map.entrySet())
+	  {
+		  System.out.println(m.getKey()+" "+m.getValue());
+	  }
+	  map.remove(6);
+	  System.out.println("Length of the map after :" + map.size());
+	   System.out.println("Iterating Hashmap...");  
+		
+	  for(Map.Entry m : map.entrySet())
+	  {
+		  System.out.println(m.getKey()+" "+m.getValue());
+	  }
+	  map.putIfAbsent(6, "Aroop");
+	  map.putIfAbsent(4, "Anoop");
+	  System.out.println("Length of the map after putifabsent :" + map.size());
+	   System.out.println("Iterating Hashmap...");  
+		
+	  for(Map.Entry m : map.entrySet())
+	  {
+		  System.out.println(m.getKey()+" "+m.getValue());
+	  }
+		  
+    }
+	@Test
+	public void listOfStudents()
+	{
+		ArrayList<Integer> studentNumbers = new ArrayList<Integer>();
+		studentNumbers.add(22);
+		ArrayList<String> studentNames = new ArrayList<String>();
+		studentNames.add("Nag");
+		ArrayList<Student> students = new ArrayList<Student>();
+		Student s1 = new Student(22,"Ram",550);
+		Student s2 = new Student(25,"Ramu",580);
+		Student s3 = new Student(28,"Ramesh",590);
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		students.add(s3);
+		students.add(new Student(38,"Raj",595)); //Run time object / Ananymous object		
+		for(int i=0;i<students.size();i++) // i=0 means first student - s1.number ,s1.name , s1.marks
+		{
+			System.out.println("Student Number :" + i);
+			System.out.println(students.get(i).number);  // i=0 means s1.number : i=1 means s2.number : i=2 means s3.number
+			System.out.println(students.get(i).name); // i=0 means s1.name : i=1 means s2.name : i=2 means s3.name
+			System.out.println(students.get(i).marks); // i=0 means s1.marks : i=1 means s2.marks : i=2 means s3.marks
+		}
+	}
+	
 	//Assignment : 
 	//Write program to reproduce the arraylist with different data types like String , Character, Boolean
 	//Write a program to sort the array / collection elements in ascending / descending order 
 	//Write some programs where you can use nested for loop. There is building Five floors are there each floor contains 30 steps . 
 		// Apply different color in each floor 
+}
+
+class Student  // Here Student itself is a type
+{
+	int number;
+	String name;
+	int marks;
+	Student(int num,String myname,int mymarks)
+	{
+		number = num;
+		name = myname;
+		marks = mymarks;
+	}
 }
